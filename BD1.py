aafaeed3b5d512d96b3e398a83b9337fc973b7f6
Unni <dominik.unnuetzer@gmx.de>
2018-10-25 16:11:51 +0200

    bd1

--- BD1.py
+++ BD1.py
@@ -1,7 +1,20 @@
+#1-2
 a = [0,1,2,3,4,5,6,7,8,9,10]
 print(a)
+
+#1-3
 b = []
 for x in a:
     if (x % 2) != 0:
         b.append(x)
 print(b)
+
+#1-4
+c = [];
+n = 10;
+i = 0;
+while i <= n:
+    c.append(i * i);
+print(c);
+
+#1-5
